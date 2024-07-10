8ee22fae164dcef13acfec0fd70e6fb2384894da
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2024-07-10 12:23:51 +0100

    Release 0.34 (#72)
    * update pytket
    
    * update changelog
    
    * update version
    
    * add docs link to readme
    
    * add link to slack and stackexchange
    
    * remove publish dosc from workflow
    
    * remove publish docs II

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     license="Apache 2",
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
-    install_requires=["pytket ~= 1.27", "pyzx ~= 0.8.0"],
+    install_requires=["pytket >= 1.30.0", "pyzx ~= 0.8.0"],
     classifiers=[
         "Environment :: Console",
         "Programming Language :: Python :: 3.10",
